2ed4c754aa8f389b4b0e01ddfab07ca3b68a5cbf
Prashant Kumar <99869977+prashantkumarrgipt@users.noreply.github.com>
2024-05-16 20:48:52 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
+echo "Building the project..."
 python3.12 pip install -r requirements.txt
+echo "Make Migration..."
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+echo "Collect Static..."
 python3.12 manage.py collectstatic --noinput --clear
